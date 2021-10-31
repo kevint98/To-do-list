7529e9b55def8da0223c876d3a45846dad603d3c
Kevin Thompson <kevinlthompson98@gmail.com>
2021-10-31 13:42:06 +0000

    Delete & Filter Tasks

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //Define UI Vars
 const form = document.querySelector('#task-form');
-const taksList = document.querySelector('.collection');
-const clearBtn = document.querySelector('.clear-tasks');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-task');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
@@ -12,6 +12,15 @@ loadEventListeners();
 function loadEventListeners() {
 	//Add task event
 	form.addEventListener('submit', addTask);
+
+	//Remove task event
+	taskList.addEventListener('click', removeTask);
+
+	//Clear task event
+	clearBtn.addEventListener('click', clearTasks);
+
+	//Filter tasks events
+	filter.addEventListener('keyup', filterTasks);
 }
 
 // Add Task
@@ -36,10 +45,44 @@ function addTask(e) {
 	li.appendChild(link);
 
 	// Append li to ul
-	taksList.appendChild(li);
+	taskList.appendChild(li);
 
 	//Clear input
 	taskInput.value = '';
 
 	e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+	if (e.target.parentElement.classList.contains('delete-item')) {
+		if (confirm('Are you sure?')) {
+			e.target.parentElement.parentElement.remove();
+		}
+	}
+}
+
+//Clear Tasks
+function clearTasks() {
+	// taskList.innerHTML = '';
+
+	//OR
+
+	while (taskList.firstChild) {
+		taskList.removeChild(taskList.firstChild);
+	}
+}
+
+//Filter Tasks
+function filterTasks(e) {
+	const text = e.target.value.toLowerCase();
+
+	document.querySelectorAll('.collection-item').forEach(function (task) {
+		const item = task.firstChild.textContent;
+		if (item.toLowerCase().indexOf(text) != -1) {
+			task.style.display = 'block';
+		} else {
+			task.style.display = 'none';
+		}
+	});
 }
